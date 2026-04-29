dbdcda6c366838ea1850fa481e6cd994e873f9da
Maxim_Simonov <simmaksimus@gmail.com>
2026-04-29 15:14:08 +0300

    idk_why_im_so_inattentive

--- DecompositionLU/source/tests.cpp
+++ DecompositionLU/source/tests.cpp
@@ -154,7 +154,7 @@ ReturnedResults TestSystem::single_reference_test(size_t n, size_t iter, const S
 ReturnedResults TestSystem::single_reference_test(size_t n, size_t iter, const SquareMatrix& sqmtr) {
 	ReturnedResults results;
 	SquareMatrix A(sqmtr);
-	lapack_int* ipiv_ptr = new int64_t[n];
+	lapack_int* ipiv_ptr = new lapack_int[n];
 
 	TP start_LU = NOW;
 	lapack_int info = LAPACKE_dgetrf(LAPACK_ROW_MAJOR, n, n, A.get_array(), n, ipiv_ptr);
