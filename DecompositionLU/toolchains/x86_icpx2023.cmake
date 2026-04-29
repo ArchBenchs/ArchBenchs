dfd8ed3285a055472c80a86ddf43aedbe06bc45e
Maxim_Simonov <simmaksimus@gmail.com>
2026-04-29 15:01:30 +0300

    fix/mkl_ref_test_on_x86_cluster

--- DecompositionLU/toolchains/x86_icpx2023.cmake
+++ DecompositionLU/toolchains/x86_icpx2023.cmake
@@ -2,3 +2,7 @@ set(CMAKE_C_COMPILER icx)
 set(CMAKE_CXX_COMPILER icpx)
 
 set(CMAKE_CXX_FLAGS "-qopenmp -g -O2 -march=icelake-server -std=c++17 ${CMAKE_CXX_FLAGS}")
+
+if(DEFINED REFERENCE_TEST && ${REFERENCE_TEST} STREQUAL "mkl")
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -qmkl=parallel")
+endif()
