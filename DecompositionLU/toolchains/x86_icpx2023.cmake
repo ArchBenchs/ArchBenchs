40fe5b81195a03d736e4aa62f603a0b616dd47ef
Maxim_Simonov <simmaksimus@gmail.com>
2026-04-29 15:18:32 +0300

    fix/undefined_reference_error_x86_cluster

--- DecompositionLU/toolchains/x86_icpx2023.cmake
+++ DecompositionLU/toolchains/x86_icpx2023.cmake
@@ -5,6 +5,6 @@ set(CMAKE_CXX_FLAGS "-qopenmp -g -O2 -march=icelake-server -std=c++17 ${CMAKE_CX
 
 if(DEFINED REFERENCE_TEST)
 	if (${REFERENCE_TEST} STREQUAL "mkl")
-		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -qmkl=parallel")
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -qmkl=parallel -DMKL_THREADING=openmp -DMKL_INTERFACE=lp64")
 	endif()
 endif()
