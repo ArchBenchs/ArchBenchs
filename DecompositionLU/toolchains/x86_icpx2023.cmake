4518c815e5626c6d0dd75ee78991cf67f3cc3ec3
Maxim_Simonov <simmaksimus@gmail.com>
2026-04-30 15:15:21 +0300

    fix/parallel_mkl_compilation

--- DecompositionLU/toolchains/x86_icpx2023.cmake
+++ DecompositionLU/toolchains/x86_icpx2023.cmake
@@ -2,9 +2,3 @@ set(CMAKE_C_COMPILER icx)
 set(CMAKE_CXX_COMPILER icpx)
 
 set(CMAKE_CXX_FLAGS "-qopenmp -g -O2 -march=icelake-server -std=c++17 ${CMAKE_CXX_FLAGS}")
-
-if(DEFINED REFERENCE_TEST)
-	if (${REFERENCE_TEST} STREQUAL "mkl")
-		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -qmkl=parallel -DMKL_THREADING=openmp -DMKL_INTERFACE=lp64")
-	endif()
-endif()
