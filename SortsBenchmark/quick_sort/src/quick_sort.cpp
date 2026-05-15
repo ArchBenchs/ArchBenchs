aa2ad777eed1fd94774f03b2e5b39041d1c05177
Eduard_Brusnigin <brusnigin27@ya.ru>
2026-05-16 00:34:19 +0300

    Final version

--- SortsBenchmark/quick_sort/src/quick_sort.cpp
+++ SortsBenchmark/quick_sort/src/quick_sort.cpp
@@ -1,26 +1,22 @@
 #include <algorithm>
 #include <omp.h>
-#include <iostream>
 
-// порог, с которого начинает запускаться сортировка вставками
-#define INSERTION_THRESHOLD (int64_t)128
-
-// порог, с которого начинает запускаться последовательная версия
-#define SEQUENTIAL_THRESHOLD (int64_t)100000
+#define INSERTION_THRESHOLD (int64_t)96
+#define SEQUENTIAL_THRESHOLD (int64_t)75000
 
 
 void insertion_sort(double* arr, int64_t left, int64_t right) {
-    for (int i = left + 1; i <= right; i++) {
-        double key = arr[i];
-        int64_t j = i - 1;
+	for (int i = left + 1; i <= right; i++) {
+		double key = arr[i];
+		int64_t j = i - 1;
 
-        while (j >= left && arr[j] > key) {
-		    arr[j + 1] = arr[j];
-		    j--;
+		while (j >= left && arr[j] > key) {
+			arr[j + 1] = arr[j];
+			j--;
 		}
 
-        arr[j + 1] = key;
-    }
+		arr[j + 1] = key;
+	}
 }
 
 
@@ -42,16 +38,16 @@ int64_t partition(double* arr, int64_t left, int64_t right) {
 
 
 	while (true) {
-        do
-            i++;
-        while (arr[i] < pivot);
+		do
+			i++;
+		while (arr[i] < pivot);
 
-        do
-            j--;
-        while (arr[j] > pivot);
-		
+		do
+			j--;
+		while (arr[j] > pivot);
 
-        if (i >= j)
+
+		if (i >= j)
 			return j;
 
 		std::swap(arr[i], arr[j]);
@@ -59,23 +55,8 @@ int64_t partition(double* arr, int64_t left, int64_t right) {
 }
 
 
-inline int64_t min(int64_t first, int64_t second) {
-	if (first < second)
-		return first;
-	return second;
-}
-
-inline int64_t max(int64_t first, int64_t second) {
-	if (first > second)
-		return first;
-	return second;
-}
-
-double sum = 0.0;
-uint64_t counter = 0;
-
 void quick_sort(double* arr, int64_t left, int64_t right) {
-	uint64_t size = right - left;
+	int64_t size = right - left;
 
 	if (size <= 0) 
 		return;
@@ -87,16 +68,6 @@ void quick_sort(double* arr, int64_t left, int64_t right) {
 
 	else if (size < SEQUENTIAL_THRESHOLD) {
 		int64_t partition_index = partition(arr, left, right);
-
-		int64_t left_size = partition_index - left + 1;
-		int64_t right_size = right - partition_index;
-
-		if (left_size > 0 && right_size > 0) {
-			double balance = (double)min(left_size, right_size) / max(left_size, right_size);
-			sum += balance;
-			counter++;
-		}
-
 
 		if (partition_index - left < right - partition_index) {
 			quick_sort(arr, left, partition_index);
@@ -111,15 +82,6 @@ void quick_sort(double* arr, int64_t left, int64_t right) {
 
 	else {
 		int64_t partition_index = partition(arr, left, right);
-
-		int64_t left_size = partition_index - left + 1;
-		int64_t right_size = right - partition_index;
-
-		if (left_size > 0 && right_size > 0) {
-			double balance = (double)min(left_size, right_size) / max(left_size, right_size);
-			sum += balance;
-			counter++;
-		}
 
 		#pragma omp task
 		quick_sort(arr, left, partition_index);
@@ -140,6 +102,4 @@ void parallel_quick_sort(double* arr, int64_t size) {
 			quick_sort(arr, 0, size - 1);
 		}
 	}
-
-	std::cout << sum / counter << std::endl;
 }
--- SortsBenchmark/quick_sort/src/test_random.cpp
+++ SortsBenchmark/quick_sort/src/test_random.cpp
@@ -5,67 +5,67 @@
 
 
 bool check_sorted(double* arr, int64_t N) {
-    for (int64_t i = 1; i < N; i++)
-        if (arr[i - 1] > arr[i])
-            return false;
+	for (int64_t i = 1; i < N; i++)
+		if (arr[i - 1] > arr[i])
+			return false;
 
-    return true;
+	return true;
 }
 
 
 void test_random() {
-    auto start_total = std::chrono::high_resolution_clock::now();
+	auto start_total = std::chrono::high_resolution_clock::now();
 
-    const int64_t N = 1000000000;
+	const int64_t N = 1000000000;
 
-    double* Array = new double[N];
+	double* Array = new double[N];
 
 
-    auto start_read = std::chrono::high_resolution_clock::now();
-    try {
-        std::ifstream file("random_data.bin", std::ios::binary);
-        file.read(reinterpret_cast<char*>(Array), N * sizeof(double));
-        file.close();
-    }
+	auto start_read = std::chrono::high_resolution_clock::now();
+	try {
+		std::ifstream file("random_data.bin", std::ios::binary);
+		file.read(reinterpret_cast<char*>(Array), N * sizeof(double));
+		file.close();
+	}
 
-    catch(...) {
-        std::cout << "Data reading error" << std::endl;
-        return;
-    }
-    auto end_read = std::chrono::high_resolution_clock::now();
+	catch(...) {
+		std::cout << "Data reading error" << std::endl;
+		return;
+	}
+	auto end_read = std::chrono::high_resolution_clock::now();
 
-    auto start_sort = std::chrono::high_resolution_clock::now();
-    parallel_quick_sort(Array, N);
-    auto end_sort = std::chrono::high_resolution_clock::now();
+	auto start_sort = std::chrono::high_resolution_clock::now();
+	parallel_quick_sort(Array, N);
+	auto end_sort = std::chrono::high_resolution_clock::now();
 
-    auto start_check = std::chrono::high_resolution_clock::now();
-    bool sorted = check_sorted(Array, N);
-    auto end_check = std::chrono::high_resolution_clock::now();
+	auto start_check = std::chrono::high_resolution_clock::now();
+	bool sorted = check_sorted(Array, N);
+	auto end_check = std::chrono::high_resolution_clock::now();
 
-    auto end_total = std::chrono::high_resolution_clock::now();
+	auto end_total = std::chrono::high_resolution_clock::now();
 
-    auto read_time = std::chrono::duration<double>(end_read - start_read).count();
-    auto sort_time = std::chrono::duration<double>(end_sort - start_sort).count();
-    auto check_time = std::chrono::duration<double>(end_check - start_check).count();
-    auto total_time = std::chrono::duration<double>(end_total - start_total).count();
+	auto read_time = std::chrono::duration<double>(end_read - start_read).count();
+	auto sort_time = std::chrono::duration<double>(end_sort - start_sort).count();
+	auto check_time = std::chrono::duration<double>(end_check - start_check).count();
+	auto total_time = std::chrono::duration<double>(end_total - start_total).count();
 
-    std::cout << "Read time: " << read_time << " s" << std::endl;
-    std::cout << "Sort time: " << sort_time << " s" << std::endl;
-    std::cout << "Check time: " << check_time << " s" << std::endl << std::endl;
-    std::cout << "Total time: " << total_time << " s" << std::endl;
+	std::cout << "Read time: " << read_time << " s" << std::endl;
+	std::cout << "Sort time: " << sort_time << " s" << std::endl;
+	std::cout << "Check time: " << check_time << " s" << std::endl << std::endl;
+	std::cout << "Total time: " << total_time << " s" << std::endl;
 
-    if (sorted)
-        std::cout << "\nArray is sorted" << std::endl;
+	if (sorted)
+		std::cout << "\nArray is sorted" << std::endl;
 
-    else
-        std::cout << "\nArray is not sorted!!!" << std::endl;
+	else
+		std::cout << "\nArray is not sorted!!!" << std::endl;
 
-    delete[] Array;
+	delete[] Array;
 }
 
 
 int main() {
-    test_random();
+	test_random();
 
-    return 0;
+	return 0;
 }
